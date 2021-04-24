9bd4e40e86ff8ffe1e2e78f07e722c3c02a00817
Barrera <barreraalexander93@gmail.com>
2021-04-24 00:57:34 -0400

    working version of ingredients dropdown

--- website/static/js/ingredients_dropdown_mod.js
+++ website/static/js/ingredients_dropdown_mod.js
@@ -1,11 +1,33 @@
-var ingredient_ctnrs = document.querySelectorAll('.ingredients_ctnr')
-if (ingredient_ctnrs){
-    for (let ctnr of ingredient_ctnrs){
-        let list_items =  ctnr.querySelectorAll('.ingredients_list')
-        for (let item of list_items){
-            console.log(item)
-            item.classList.add('dissolver')
-            // item.style = "opacity:0;position:absolute;"
-        }
+// var ingredient_ctnrs = document.querySelectorAll('.ingredients_ctnr')
+// if (ingredient_ctnrs){
+//     for (let ctnr of ingredient_ctnrs){
+//         let list_items =  ctnr.querySelectorAll('.ingredients_list')
+//         for (let item of list_items){
+//             console.log(item)
+//             item.classList.add('dissolver')
+//             // item.style = "position:absolute;"
+//             // item.style = "opacity:0;position:absolute;"
+//         }
+//     }
+// }
+
+
+var ing_triangle = document.querySelectorAll('.ing_triangle')
+if (ing_triangle){
+    for (let tri of ing_triangle){
+        tri.addEventListener('click', function(){
+            let ingredients_list_id = "ingredients"+tri.getAttribute("data-loop_index")
+            let ing_list_elem = document.querySelector(`#${ingredients_list_id}`)
+            console.log(ing_list_elem)
+            if (tri.getAttribute("data-is_open")==0){
+                tri.style = "transform:rotate(0deg)"
+                tri.setAttribute("data-is_open", 1)
+                ing_list_elem.classList.remove('invisible')
+            } else {
+                tri.style = "transform:rotate(270deg)"
+                tri.setAttribute("data-is_open", 0)
+                ing_list_elem.classList.add('invisible')
+            }
+        })
     }
 }
