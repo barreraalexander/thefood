564a32a60057e81dc070866615de4bbe2bea3736
Barrera <barreraalexander93@gmail.com>
2021-04-24 01:02:30 -0400

    clean up drop down

--- website/static/js/ingredients_dropdown_mod.js
+++ website/static/js/ingredients_dropdown_mod.js
@@ -1,17 +1,3 @@
-// var ingredient_ctnrs = document.querySelectorAll('.ingredients_ctnr')
-// if (ingredient_ctnrs){
-//     for (let ctnr of ingredient_ctnrs){
-//         let list_items =  ctnr.querySelectorAll('.ingredients_list')
-//         for (let item of list_items){
-//             console.log(item)
-//             item.classList.add('dissolver')
-//             // item.style = "position:absolute;"
-//             // item.style = "opacity:0;position:absolute;"
-//         }
-//     }
-// }
-
-
 var ing_triangle = document.querySelectorAll('.ing_triangle')
 if (ing_triangle){
     for (let tri of ing_triangle){
